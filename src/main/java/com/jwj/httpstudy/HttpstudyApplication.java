4a6a44de26bbdec4a45d4896362289e13e0f8bb7
w32807 <w32807@nate.com>
2022-04-15 23:23:31 +0900

    First Commit

---
+++ src/main/java/com/jwj/httpstudy/HttpstudyApplication.java
@@ -0,0 +1,13 @@
+package com.jwj.httpstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwj/httpstudy/HttpstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwj.httpstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
